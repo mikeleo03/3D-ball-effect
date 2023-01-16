99825fc58eea3762604fda4cc7470f1487620e9f
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-01-16 20:38:39 +0700

    First 3D Ball shapes
    First 3D Ball shapes

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
 import * as THREE from 'three';
+import './style.css'
 
 // scene
 const scene = new THREE.Scene();
 
 // create a sphere
+const geometry = new THREE.SphereGeometry(3, 64, 64)
+const material = new THREE.MeshStandardMaterial({
+  color: "#00ff83",
+})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// sizes of viewport
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+
+// light
+const light = new THREE.PointLight(0xffffff, 1, 100)
+light.position.set(0, 10, 10)
+scene.add(light)
+
+// camera
+const camera = new THREE.PerspectiveCamera(45, sizes.width/sizes.height, 0.1, 100)
+camera.position.z = 20
+scene.add(camera)
+
+// Renderer
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({ canvas })
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
