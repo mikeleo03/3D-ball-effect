b3d1631a47e1046e38b612ad155dd79c53548039
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-01-16 23:30:20 +0700

    (another) Resolving package
    (another) Resolving package

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 import './style.css'
 import gsap from "gsap";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls"
