089dadcadd10cdcb100944d1a0a999f5e0ee7921
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-01-16 22:18:28 +0700

    Finally did it
    Finally did it

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import './style.css'
+import gsap from "gsap";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls"
 
 // scene
@@ -9,6 +10,7 @@ const scene = new THREE.Scene();
 const geometry = new THREE.SphereGeometry(3, 64, 64)
 const material = new THREE.MeshStandardMaterial({
   color: "#00ff83",
+  roughness: 0.5,
 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
@@ -22,6 +24,7 @@ const sizes = {
 // light
 const light = new THREE.PointLight(0xffffff, 1, 100)
 light.position.set(0, 10, 10)
+light.intensity = 1.25
 scene.add(light)
 
 // camera
@@ -51,8 +54,8 @@ window.addEventListener('resize', () => {
   sizes.height = window.innerHeight;
 
   // update camera
-  camera.updateProjectionMatrix()
   camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
   renderer.setSize(sizes.width, sizes.height)
 })
 
@@ -64,3 +67,32 @@ const loop = () => {
 }
 loop()
 
+// Beautiful timeline effect :D
+const tl = gsap.timeline({ defaults : { duration: 1} })
+tl.fromTo(mesh.scale, {z : 0, y : 0, x : 0}, {z : 1, y : 1, x : 1})
+tl.fromTo("nav", {y : "-100%"}, {y : "0%"})
+tl.fromTo(".title", {opacity : 0}, {opacity : 1})
+
+// Mouse animation color
+let mousedown = false
+let rgb = []
+window.addEventListener("mousedown", () => (mousedown = true))
+window.addEventListener("mouseup", () => (mousedown = false))
+
+window.addEventListener("mousemove", (e) => {
+  if (mousedown) {
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.height) * 255),
+      150,
+    ]
+
+    // Animation process
+    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`)
+    gsap.to(mesh.material.color, {
+      r: newColor.r,
+      g: newColor.g,
+      b: newColor.b,
+    })
+  }
+})
